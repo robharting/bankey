695fb24d22867bc0f554fb70d2ee6b9f629f1e6b
Rob Harting <Rob.Harting@ing.com>
2025-09-12 11:17:01 +0200

    initial setup

---
+++ Bankey/Login/LoginViewController.swift
@@ -0,0 +1,39 @@
+//
+//  ViewController.swift
+//  Bankey
+//
+//  Created by Harting, R.P.G. (Rob) on 18/08/2025.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+    
+    let loginView = LoginView()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        style()
+        layout()
+    }
+
+}
+
+extension LoginViewController {
+    private func style(){
+        loginView.translatesAutoresizingMaskIntoConstraints = false
+        
+    }
+    
+    private func layout() {
+        view.addSubview(loginView)
+        
+        NSLayoutConstraint.activate([
+            loginView.centerYAnchor.constraint(equalTo: view.centerYAnchor),
+            loginView.leadingAnchor.constraint(equalToSystemSpacingAfter: view.leadingAnchor, multiplier: 1),
+            view.trailingAnchor.constraint(equalToSystemSpacingAfter: loginView.trailingAnchor, multiplier: 1),
+            ])
+
+    }
+}
+
