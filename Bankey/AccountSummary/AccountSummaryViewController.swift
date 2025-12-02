20ff8a6afd4c23018bbc358012deeb9933005d64
Rob Harting <Rob.Harting@ing.com>
2025-12-02 09:47:44 +0100

    feat: Add view model

--- Bankey/AccountSummary/AccountSummaryViewController.swift
+++ Bankey/AccountSummary/AccountSummaryViewController.swift
@@ -9,13 +9,7 @@ import UIKit
 
 class AccountSummaryViewController: UIViewController {
     
-    let games = [
-        "Pacman",
-        "Space Invaders",
-        "Space Patrol",
-        "Pacman II"
-    ]
-    
+    var accounts: [AccountSummaryCell.ViewModel] = []
     var tableView = UITableView()
     
     override func viewDidLoad() {
@@ -24,10 +18,12 @@ class AccountSummaryViewController: UIViewController {
     }
 }
 
+// MARK: - Setup
 extension AccountSummaryViewController {
     private func setup() {
         setupTableView()
         setupTableViewHeader()
+        fetchData()
     }
     
     private func setupTableView() {
@@ -49,34 +45,62 @@ extension AccountSummaryViewController {
             tableView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor)
         ])
     }
-
+    
     private func setupTableViewHeader() {
         let header = AccountSummaryHeaderView(frame: .zero)
-
+        
         // the height
         var size = header.systemLayoutSizeFitting(UIView.layoutFittingCompressedSize)
         size.width = UIScreen.main.bounds.width
         // the width
         header.frame.size = size
-
+        
         tableView.tableHeaderView = header
-
+        
     }
 }
 
+// MARK: - UITableViewDataSource
 extension AccountSummaryViewController: UITableViewDataSource {
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: AccountSummaryCell.reuseID, for: indexPath) as! AccountSummaryCell
+        guard !accounts.isEmpty else { return UITableViewCell() }
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier:
+                                                    AccountSummaryCell.reuseID, for: indexPath) as! AccountSummaryCell
+        let account = accounts[indexPath.row]
+        cell.configure(with: account)
+        
         return cell
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return games.count
+        return accounts.count
     }
 }
 
+// MARK: - UITableViewDelegate
 extension AccountSummaryViewController: UITableViewDelegate {
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         
     }
+}
+
+extension AccountSummaryViewController {
+    private func fetchData() {
+        
+        let savings = AccountSummaryCell.ViewModel(
+            accountType: .Banking,
+            accountName: "Savings Account")
+        let visa = AccountSummaryCell.ViewModel(
+            accountType: .CreditCard,
+            accountName: "Visa Avion Credit Card")
+        let investment = AccountSummaryCell.ViewModel(
+            accountType: .Investment,
+            accountName: "Tax-Free Saver Investment")
+        
+        accounts.append(savings)
+        accounts.append(visa)
+        accounts.append(investment)
+    }
+    
 }
